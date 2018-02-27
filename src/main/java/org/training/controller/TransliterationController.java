abe44cb34ff6b6e6c8b1b44b0a4f6fa10f1d416a
Tetiana_Fomitska <Tetiana_Fomitska@epam.com>
2018-02-27 17:05:54 +0200

    mend

--- src/main/java/org/training/controller/TransliterationController.java
+++ src/main/java/org/training/controller/TransliterationController.java
@@ -1,19 +1,15 @@
 package org.training.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.training.service.TransliterationService;
 
-import java.util.Collections;
 import java.util.Map;
-import java.util.Objects;
 
 @RestController
 public class TransliterationController {
