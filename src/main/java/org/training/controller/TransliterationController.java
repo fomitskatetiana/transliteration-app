abd1cc9fef78d00b9de6847e6af5c09aab5c4e21
Tetiana_Fomitska <Tetiana_Fomitska@epam.com>
2018-02-27 17:04:53 +0200

    TransliterationController: added method for transliteration json data

--- src/main/java/org/training/controller/TransliterationController.java
+++ src/main/java/org/training/controller/TransliterationController.java
@@ -1,26 +1,39 @@
 package org.training.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.training.service.TransliterationService;
 
+import java.util.Collections;
 import java.util.Map;
+import java.util.Objects;
 
 @RestController
 public class TransliterationController {
+
     private final TransliterationService transliterationService;
+    private static final String PARAMETER_NAME = "names";
 
     @Autowired
-    public TransliterationController (TransliterationService transliterationService) {
+    public TransliterationController(TransliterationService transliterationService) {
         this.transliterationService = transliterationService;
     }
 
-    @RequestMapping(method = RequestMethod.POST)
-    Map<String, String> transliterate(@RequestParam String names) {
+    @PostMapping(params = PARAMETER_NAME)
+    Map<String, String> transliterateFormData(@RequestParam String names) {
         return transliterationService.convertUkrainianToEnglish(names);
+    }
+
+    @PostMapping
+    Map<String, String> transliterateJsonData(@RequestBody Map<String, String> names) {
+        return transliterationService.convertUkrainianToEnglish(names.get(PARAMETER_NAME));
     }
 
     @RequestMapping(method = RequestMethod.GET)
